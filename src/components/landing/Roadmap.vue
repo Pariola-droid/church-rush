c20fd6218feb21757ab955a8677fd37962972333
Pariola <kehindephilip15@gmail.com>
2021-12-09 01:45:48 +0100

    done with the roadmap section

--- src/components/landing/Roadmap.vue
+++ src/components/landing/Roadmap.vue
@@ -17,20 +17,84 @@
           </div>
 
           <div class="i_wrap">
-                <div class="item i1">
-              
+                <div class="item i1">                
+                    <div class="fills">
+                        <h5>
+                            Roll Out 1<br/>Content Population
+                        </h5>
+                        <p class="p">
+                            January 5th, 2021
+                        </p>
+                        <p class="pp">
+                            These are for churches that filled the get notified form between November, 20th and December, 20th.
+                        </p>
+                    </div>
+
+                    <div class="icon">
+                        <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M42 42H26V12C26 10.4087 26.6321 8.88258 27.7574 7.75736C28.8826 6.63214 30.4087 6 32 6H42C42.5304 6 43.0391 6.21071 43.4142 6.58579C43.7893 6.96086 44 7.46957 44 8V40C44 40.5304 43.7893 41.0391 43.4142 41.4142C43.0391 41.7893 42.5304 42 42 42ZM22 42H6C5.46957 42 4.96086 41.7893 4.58579 41.4142C4.21071 41.0391 4 40.5304 4 40V8C4 7.46957 4.21071 6.96086 4.58579 6.58579C4.96086 6.21071 5.46957 6 6 6H16C17.5913 6 19.1174 6.63214 20.2426 7.75736C21.3679 8.88258 22 10.4087 22 12V42ZM22 42H26V46H22V42Z" fill="#311747"/>
+                        </svg>
+                    </div>
                 </div>
 
                 <div class="item i2">
+                    <div class="icon">
+                        <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M10 4H38C38.5304 4 39.0391 4.21071 39.4142 4.58579C39.7893 4.96086 40 5.46957 40 6V42C40 42.5304 39.7893 43.0391 39.4142 43.4142C39.0391 43.7893 38.5304 44 38 44H10C9.46957 44 8.96086 43.7893 8.58579 43.4142C8.21071 43.0391 8 42.5304 8 42V6C8 5.46957 8.21071 4.96086 8.58579 4.58579C8.96086 4.21071 9.46957 4 10 4ZM24 34C23.4696 34 22.9609 34.2107 22.5858 34.5858C22.2107 34.9609 22 35.4696 22 36C22 36.5304 22.2107 37.0391 22.5858 37.4142C22.9609 37.7893 23.4696 38 24 38C24.5304 38 25.0391 37.7893 25.4142 37.4142C25.7893 37.0391 26 36.5304 26 36C26 35.4696 25.7893 34.9609 25.4142 34.5858C25.0391 34.2107 24.5304 34 24 34Z" fill="#311747"/>
+                        </svg>
+                    </div>
 
+                    <div class="fills">
+                        <h5>
+                            Roll Out 2<br/>Mobile Apps
+                        </h5>
+                        <p class="p">
+                            March, 2022
+                        </p>
+                        <p class="pp">
+                            Individuals will be able to download the app from the app and play store.
+                        </p>
+                    </div>                    
                 </div>
 
                 <div class="item i3">
+                    <div class="fills">
+                        <h5>
+                            Roll Out 3<br/>Church Access
+                        </h5>
+                        <p class="p">
+                            June, 2022
+                        </p>
+                        <p class="pp">
+                            Opened to all churches that filled the get notified form between December, 6th 2021 and March, 1st 2022.
+                        </p>
+                    </div>
 
+                    <div class="icon">
+                        <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M26 20H40L22 46V28H8L26 2V20Z" fill="#311747"/>
+                        </svg>
+                    </div>
                 </div>
 
                 <div class="item i4">
+                    <div class="icon">
+                        <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M4.1001 26H15.0541C15.3963 32.3337 17.4152 38.4628 20.9041 43.76C16.534 43.0735 12.5145 40.9583 9.47413 37.745C6.43377 34.5316 4.54393 30.4014 4.1001 26ZM4.1001 22C4.54393 17.5986 6.43377 13.4683 9.47413 10.255C12.5145 7.04169 16.534 4.92644 20.9041 4.23999C17.4152 9.53721 15.3963 15.6663 15.0541 22H4.1001ZM43.9001 22H32.9461C32.6039 15.6663 30.585 9.53721 27.0961 4.23999C31.4662 4.92644 35.4857 7.04169 38.5261 10.255C41.5664 13.4683 43.4563 17.5986 43.9001 22ZM43.9001 26C43.4563 30.4014 41.5664 34.5316 38.5261 37.745C35.4857 40.9583 31.4662 43.0735 27.0961 43.76C30.585 38.4628 32.6039 32.3337 32.9461 26H43.9021H43.9001ZM19.0601 26H28.9401C28.6109 31.399 26.9107 36.6249 24.0001 41.184C21.0895 36.6249 19.3893 31.399 19.0601 26ZM19.0601 22C19.3893 16.601 21.0895 11.3751 24.0001 6.81599C26.9107 11.3751 28.6109 16.601 28.9401 22H19.0601Z" fill="#311747"/>
+                        </svg>
+                    </div>
 
+                    <div class="fills">
+                        <h5>
+                            Roll Out 4<br/>General
+                        </h5>
+                        <p class="p">
+                            September, 2022
+                        </p>
+                        <p class="pp">
+                            Here we open the platform to all churches and fellowships.
+                        </p>
+                    </div>  
                 </div>
           </div>
       </div>  
@@ -129,23 +193,100 @@
 }
 
 .road .i_wrap .item {
-    width: 444px;
-    height: 204px;
-    background: rgba(255, 0, 0, 0.219);
+    width: 560px;
+    height: 260px;
+    /* background: rgba(255, 0, 0, 0.219); */
     position: absolute;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
 }
 
+.road .i_wrap .item .icon {
+    height: 120px;
+    width: 120px;
+    background: rgba(255, 204, 0, 1);
+    border-radius: 100%; 
+    position: absolute;
+    display: grid;
+    place-items: center;
+}
+
+.road .i_wrap .item .fills {
+    padding-top: 55px;
+}
+
+.road .i_wrap .item .fills h5 {
+    font-family: 'Inter', sans-serif;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 24px;
+    line-height: 32px;
+    color: #FFCC00;
+}
+
+.road .i_wrap .item .fills .p {
+    font-family: 'Poppins', sans-serif;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 32px;
+    color: #FFFFFF;
+}
+
+.road .i_wrap .item .fills .pp {
+    font-family: 'Poppins', sans-serif;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 32px;
+    color: #FFFFFF;
+    max-width: 444px;
+}
+
+
+/* item 1 - 4 */
 .road .i_wrap .i1 {
-    
+    top: 320px;
+    left: 50px;    
 }
+.road .i_wrap .i1 .icon {
+    right: -30px;
+    top: -30px;
+}
+
+
 .road .i_wrap .i2 {
-   
+    top: 650px;
+    right: -15px;  
 }
+.road .i_wrap .i2 .icon {
+    left: -30px;
+    top: -30px;
+}
+.road .i_wrap .i2 .fills {
+   margin-left: auto;
+}
+
 .road .i_wrap .i3 {
-    
+    top: 1000px;
+    left: 50px;
 }
+.road .i_wrap .i3 .icon {
+    right: -30px;
+    top: -30px;
+}
+
 .road .i_wrap .i4 {
-  
+    top: 1320px;
+    right: -15px; 
+}
+.road .i_wrap .i4 .icon {
+    left: -30px;
+    top: -30px;
+}
+.road .i_wrap .i4 .fills {
+   margin-left: auto;
 }
 
 
